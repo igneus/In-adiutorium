0e254eb0c1af6be18f9a2bdfd0618c59641eab8b
Jakub Pavlík jn <severus@post.cz>
2012-01-04 17:59:55 +0100

    headers get read properly

--- nastroje/musicreader.rb
+++ nastroje/musicreader.rb
@@ -2,6 +2,7 @@ class LilyPondScore
   def initialize(text)
     @text = text
     init_lyrics
+    init_header
   end
   
   attr_reader :text
@@ -30,6 +31,56 @@ class LilyPondScore
     @lyrics_readable.gsub!(/\\\w+/, '') # LilyPond variables
     @lyrics_readable.gsub!(/\s+/, ' ') # whitespace
     @lyrics_readable.strip! # leading and trailing whitespace
+  end
+  
+  def init_header
+    @header = {}
+    i1 = @text.index '\header'
+    unless i1
+      puts "no header"
+      return
+    end
+    i1 = @text.index '{', i1
+    i2 = index_matching_brace @text, i1
+    htext = @text[i1+1..i2-1]
+    hlines = htext.split "\n"
+    hlines.each do |l|
+      l.strip!
+      if ! l.index '=' then
+        next
+      end
+      ii = l.index '='
+      name = l[0..ii-1].strip
+      value = l[ii+1..-1].strip
+      value.gsub!(/['"]/, '') # strip quotes
+      @header[name] = value
+    end
+  end
+  
+  # finds index of a brace matching to a brace at index i1
+  def index_matching_brace(str, i1)
+    braces_stack = [i1]
+    i = i1
+    loop do
+      io = str.index '{', i
+      ic = str.index '}', i
+      
+      unless ic
+        raise "No more closing brace found in the given string, #{braces_stack.size} braces still open."
+      end
+      
+      if io &&  io < ic then
+        braces_stack.push io
+        i = io+1
+      else
+        braces_stack.pop
+        i = ic+1
+      end
+      
+      if braces_stack.empty? then
+        return ic
+      end
+    end
   end
 end
 
