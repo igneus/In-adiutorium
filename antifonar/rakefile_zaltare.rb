9c5536003ac29b6ccb2a62408768a52ddd720cd3
Jakub Pavlík jn <severus@post.cz>
2014-04-20 16:43:27 +0200

    psalter: rake: remove lists of psalms and canticles
    these are instead loaded from the tex file

--- antifonar/rakefile_zaltare.rb
+++ antifonar/rakefile_zaltare.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Psalters without music
 
+require 'set'
+
 #######################
 # zaltar
 
@@ -12,81 +14,27 @@ options_zaltar = $commonoptions+$o_warnmarks
 canticleoptions_zaltar = $canticleoptions+$o_warnmarks
 magnificatoptions_zaltar = $commonoptions_withoutdoxology+$o_doxology_full+$o_warnmarks
 
-cislazalmu_zaltar = %w( 95 100 67 24
+cislazalmu_zaltar = Set.new
+kantika_zaltar = Set.new
 
-                  141 142 1 2 3 63 149 118i 118ii 118iii 110 114
-                  5 29 19b 7i 7ii 11 15
-                  33 119alef 13 14 20 21
-                  36 47 119beth 17i 17ii 27i 27ii
-                  57 48 119gimel 25i 25ii 30 32
-                  51 119dalet 26 28 41 46
-                  119kof 117 119he 34i 34ii
-                  
-                  119nun 16 118 150 23 76i 76ii 115
-                  42 19a 119vau 40i 40ii 45i 45ii
-                  43 65 119zajin 53 54 49i 49ii
-                  77 97 119chet 55i 55ii 62
-                  80 81 119tet 56 57 72i 72ii
-                  147ii 119jod 59 60 116i 121
-                  92 8 119kaf 61 64
-                  
-                  113 116ii 93 148 111
-                  84 96 119lamed 71i 71ii 123 124
-                  85 119mem 74i 74ii 125 131
-                  86 98 70 75 126 127
-                  87 99 119samech 79 132i 132ii
-                  22i 22ii 22iii 135i 135ii
-                  119ajin
-                  
-                  122 130 112
-                  90 119pe 82 120 136i 136ii
-                  101 144 119sade 88i 88ii 137 138
-                  108 146 94i 94ii 139i 139ii
-                  143 147i 119res 128 129 144i 144ii
-                  119sin 133 140 145i 145ii
-                  119tau
+# read list of psalms and canticles used
+File.open('antifonar_zaltar.tex') do |fr|
+  fr.each_line do |l|
+    match = /\\(zalm|kantikum)\{([^\}]+)\}/.match(l)
+    next if match.nil?
 
-                  30i 30ii 86i 96i 96ii
-
-                  4 134 91 86 143 31 130 16 88
-                  ) + %w(
-                  1 2 3
-                  6 9i 9ii
-                  10i 10ii 12
-                  18i 18ii 18iii
-                  18iv 18v 18vi
-                  35i 35ii 35iii
-                  131 132i 132ii
-                  105i 105ii 105iii
-
-                  104i 104ii 104iii
-                  31i 31ii 31iii
-                  37i 37ii 37iii
-                  39i 39ii 52
-                  44i 44ii 44iii
-                  38i 38ii 38iii
-                  136i 136cii 136ciii
-                  106i 106ii 106iii
-
-                  145ci 145cii 145ciii
-                  50i 50ii 50iii
-                  68i 68ii 68iii
-                  89i 89ii 89iii
-                  89iv 89v 90
-                  69i 69ii 69iii
-                  107i 107ii 107iii
-
-                  24 66i 66ii
-                  73i 73ii 73iii
-                  102i 102ii 102iii
-                  103i 103ii 103iii
-
-                  55ci 55cii 55ciii
-                  78i 78ii 78iii
-                  78iv 78v 78vi
-
-                  33ci 33cii 92ci 92cii 103ci 103cii
-                  )
+    if match[1] == 'zalm' then
+      cislazalmu_zaltar << match[2]
+    elsif match[1] == 'kantikum' then
+      kantika_zaltar << match[2]
+    else
+      # unexpected
+      raise RuntimeError.new(match[1])
+    end
+  end
+end
+# these canticles must be treated separately, see below
+kantika_zaltar -= ['dan3iii', '1petr2', 'benedictus', 'magnificat', 'nuncdimittis']
 
 cislazalmu_zaltar.each do |z|
   zalmyzaltare << genzalm("zalm"+z+".zalm", options_zaltar, adresar_zaltar)
@@ -98,20 +46,8 @@ dan3iiioptions = $commonoptions_withoutdoxology+$o_canticletitle+" --output-appe
 zalmyzaltare << genzalm('kantikum_dan3iii.zalm', dan3iiioptions, adresar_zaltar)
 # Te Deum, taky bez doxologie
 zalmyzaltare << genzalm('tedeum.zalm', $commonoptions_withoutdoxology+' --skip-title', adresar_zaltar)
-# ostatni kantika
-%w( fp2 ef1 zj4 kol1 zj11 zj15
 
-1kron29 tob13i jdt16 jer31 iz45 ex15 
-dan3ii sir36 iz38 1sam2 iz12 hab3 
-dt32 iz2 iz26 iz33ii iz40ii jer14 mdr9
-iz42 dan3i iz61 iz66 tob13ii ez36 
-
-iz33i sir36b tob13cii tob13ciii 
-iz2ci jer7 iz61cii iz62 sir39 
-iz61ci mdr3i mdr3ii mdr10 
-jer17 sir14 sir31 
-iz49 iz40i plac5 iz63 oz6 sof3 iz9 
-pr9 mdr16 1sam2i 1sam2ii ).each do |kk| 
+kantika_zaltar.each do |kk| 
   zalmyzaltare << genzalm("kantikum_#{kk}.zalm", canticleoptions_zaltar, adresar_zaltar)
 end
 # this canticle is special: it needs a special title, because it's preceded by a rubric.
