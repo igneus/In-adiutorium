b9c38094ac17cc3f534c3f90cd7113a0f8e05989
Jakub Pavlik <severus@post.cz>
2019-12-21 23:40:42 +0100

    commit long uncommitted file essential for updatefromvar.rb
    there's been sort of mess in the repository I was used to work with
    most, with lots of files not intended to be committed,
    and issues like this are the result

---
+++ nastroje/lib/updatefromvar/development_clean.rb
@@ -0,0 +1,35 @@
+# utilities cleaning lilypond source from development annotations
+# and other stuff that is either insignificant for detection of
+# changes or unsuitable for copying to production
+module DevelopmentClean
+  # removes all development annotations
+  def clean_score(lily_src)
+    remove_marks \
+      remove_markers \
+      remove_trailing_whitespace \
+      remove_variable_assignment lily_src
+  end
+
+  def remove_marks(lily_src)
+    lily_src.gsub(/\\mark\\sipka\s*/, '')
+  end
+
+  def remove_markers(lily_src)
+    # remove whole lines
+    lily_src.gsub(/(^\s*)?\\zvyraznovac\w+(\s*\n)?/m, '')
+  end
+
+  # if the score as a whole is assigned to a variable,
+  # remove the assignment
+  def remove_variable_assignment(lily_src)
+    lily_src.sub(/\A\s*\w+\s*=\s*/, '')
+  end
+
+  def remove_trailing_whitespace(lily_src)
+    lily_src.gsub(/\s+$/, "\n")
+  end
+
+  def remove_comments(lily_src)
+    lily_src.gsub(/%.*$/, '')
+  end
+end
