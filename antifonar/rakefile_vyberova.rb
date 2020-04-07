bb586e1be67e3b7201810c16228bdc326d2ab816
Jakub Pavlik <severus@post.cz>
2020-04-07 19:58:40 +0200

    Triduum book: vigils

--- antifonar/rakefile_vyberova.rb
+++ antifonar/rakefile_vyberova.rb
@@ -25,6 +25,9 @@ zalmy_triduum = []
 120 121 122 123 124 125 126 127 128 ).each do |z|
   zalmy_triduum << genzalm("zalm"+z+".zalm", options_triduum, adresar_triduum)
 end
+zalmy_triduum +=
+  %w(kantikum_jer14.zalm kantikum_ez36.zalm kantikum_plac5.zalm) \
+    .collect {|i| genzalm(i, options_triduum, adresar_triduum) }
 zalmy_triduum << genspojenyzalm(['zalm40i.zalm', 'zalm40ii.zalm'], 'zalm40.tex', options_triduum+" --title-template '\\nadpisZalmu{Žalm 40, 2-14.17-18}'", adresar_triduum)
 zalmy_triduum << genspojenyzalm(['zalm27i.zalm', 'zalm27ii.zalm'], 'zalm27.tex', options_triduum+" --title-template '\\nadpisZalmu{Žalm 27}'", adresar_triduum)
 zalmy_triduum << genspojenyzalm(['zalm76i.zalm', 'zalm76ii.zalm'], 'zalm76.tex', options_triduum+" --title-template '\\nadpisZalmu{Žalm 76}'", adresar_triduum)
