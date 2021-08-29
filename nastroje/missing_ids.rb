3506630001e99aba73eaaafacf4c7e274091a051
Jakub Pavlik <severus@post.cz>
2021-08-29 18:15:43 +0200

    missing_ids.rb: --verbose; exit status 1 if errors found

--- nastroje/missing_ids.rb
+++ nastroje/missing_ids.rb
@@ -1,12 +1,31 @@
 # Accepts a list of path, prints scores missing an ID.
 
+require 'optparse'
+
 require 'lyv'
+
+verbose = false
+OptionParser.new do |opts|
+  opts.on '-v', '--verbose', 'verbose output' do
+    verbose = true
+  end
+end.parse!
+
+missing_found = false
 
 ARGV.each do |path|
   Lyv::LilyPondMusic.new(path).scores.each do |score|
     id = score.header['id']
     if id.nil? || id == ''
+      missing_found = true
       puts "#{path}##{score.number} #{score.lyrics_readable}"
+
+      if verbose
+        puts score.text
+        puts
+      end
     end
   end
 end
+
+exit 1 if missing_found
