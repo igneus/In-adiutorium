bcfc930f254df48c40166af67d637872d4d110ab
Jakub Pavlik <severus@post.cz>
2022-01-04 02:50:12 +0100

    simplify code

--- antifonar/skripty/listofpsalms.rb
+++ antifonar/skripty/listofpsalms.rb
@@ -17,33 +17,38 @@ require 'optparse'
 require 'yaml'
 require_relative 'pagerefoptimal'
 
-IUNIT = 2 # two spaces as indentation unit
-
 # patterns for sorting according to standard suffixes
 $hebrew_alphabet = %w( alef beth gimel dalet he vau zajin chet tet jod kaf
 $lamed mem nun samech ajin pe sade res sin tau )
 $roman_numbers = %w( i ii iii iv v vi vii viii ix x xi xii )
 
-$psalmname_re = /(?<num>\d+)(?<suff>\w*)/
-$canticlename_re = /(?<booknum>\d*)(?<bookcode>\D+)(?<chapter>\d+)(?<suff>\w*)/
+$psalmname_re = /^(?<num>\d+)(?<suff>\w*)$/
+$canticlename_re = /^(?<booknum>\d*)(?<bookcode>\D+)(?<chapter>\d+)(?<suff>\w*)$/
 
 $canticles_number_in_name = ['1sam', '1kron', '1petr', '1tim']
+
+# where the book sigle can't be prettified by mere capitalization
+$canticles_with_diacritics = {'pr' => 'Př', 'plac' => 'Pláč'}
+
+# string tokens translating particular LaTeX code
+$special_codes = {
+  'rchne1t' => '\laudyNedelePrvnihoTydne',
+}
 
 
 
 def section_title(line)
   puts
-  puts "\\subsection{#{line.strip}}"
+  puts "\\subsection{#{line}}"
 end
 
 def occasion_title(line)
   puts
-  puts "\\paragraph{#{line.strip}}"
+  puts "\\paragraph{#{line}}"
 end
 
 def hour_title(line)
-  t = line.strip
-  puts case t
+  puts case line
        when "1. nešpory"
          "\\idxNesporyI"
        when "modlitba se čtením"
@@ -68,43 +73,33 @@ def tokenized_hour_content(value)
   
   value.collect do |rt|
     if rt.is_a? Fixnum
+      # psalm number
       [:ps, rt.to_s]
-    elsif rt =~ /^\(.+?\)/
-      [:txt, rt.strip[1..-2]] # the braced text
+    elsif $special_codes.has_key? rt
+      # placeholder for a piece of LaTeX code from $special_codes
+      [:code, rt]
+    elsif rt =~ /^\(.+?\)$/
+      # piece of text to be printed in the index
+      [:txt, rt[1..-2]]
     else
-      # simple token - number of a psalm or a code of a canticle
+      # number of a psalm or sigle of a canticle
       is_canticle =
         $canticles_number_in_name.find {|c| rt.start_with? c} ||
-        rt !~ /^\d+\w*$/
-      [:ps, rt.strip, is_canticle ? :canticle : nil]
+        rt !~ $psalmname_re
+      [:ps, rt, is_canticle ? :canticle : nil]
     end
   end
 end
 
 def hour_content(value, label_index_lookup)
   tokens = tokenized_hour_content(value)
-  
-  psalms = []
-  
 
   # search for best combination of text references
   # (some psalms reoccur in the psalter and we don't like page-flipping)
-  labels = []
-  tokens.each do |t|
-    if t[0] != :ps then
-      next
-    end
-
-    if t[1] == "rchne1t" then
-      # skip
-    elsif t[2] == :canticle then
-      # canticle
-      labels << ("k"+t[1])
-    else
-      # psalm
-      labels << ("z#{t[1]}")
-    end
-  end
+  labels =
+    tokens
+      .select {|t| t[0] == :ps }
+      .collect {|t| (t[2] == :canticle ? 'k' : 'z') + t[1] }
   label_nums = {}
   if labels.size >= 2 then
     label_nums_array = label_index_lookup.shortestn(*labels)
@@ -115,21 +110,20 @@ def hour_content(value, label_index_lookup)
 
   tokens.each_with_index do |t,ti|
     case t[0]
+    when :code
+      puts $special_codes[t[1]]
+      puts '. '
     when :ps
-      if t[1] == "rchne1t" then
-        # puts "Žalmy nedělní z 1. týdne, str. \\pageref{zalmyne1trch}"
-        puts "\\laudyNedelePrvnihoTydne"
-      elsif t[2] == :canticle
+      if t[2] == :canticle
         # canticle
         sigle = canticle_name_pretty t[1]
-        
         print "\\textRef{k#{t[1]}:#{label_nums['k'+t[1]]}}{#{sigle}}"
       else
         # psalm
         prettyt = psalm_name_pretty t[1]
         print "\\textRef{z#{t[1]}:#{label_nums['z'+t[1]]}}{Žalm #{prettyt}}"
-        psalms << t[1]
       end
+
       # for both psalms and canticles:
       if ti != (tokens.size - 1) && tokens[ti+1][0] != :txt then
         puts "; "
@@ -140,11 +134,9 @@ def hour_content(value, label_index_lookup)
       puts "\\rubr{#{t[1]}}"
     end
   end
-  
-  return psalms
 end
 
-# gets psalm code lik '15', '19a' or '119bet' und makes it to a pretty one
+# gets psalm code like '15', '19a' or '119bet' und makes it pretty
 def psalm_name_pretty(p)
   pp = p.match($psalmname_re)
 
@@ -152,14 +144,10 @@ def psalm_name_pretty(p)
 end
  
 def canticle_name_pretty(c)
-  # book names which can't be retrieved by a simple
-  # transformation of the sigle
-  special_booknames = {'pr' => 'Př', 'plac' => 'Pláč'}
-
   cp = c.match $canticlename_re
 
   book = cp[:bookcode]
-  book = special_booknames[book] || book.capitalize
+  book = $canticles_with_diacritics[book] || book.capitalize
   
   sigle = book + ' ' + cp[:chapter]
   sigle = cp[:booknum] + ' ' + sigle if cp[:booknum] != ''
@@ -207,8 +195,6 @@ unless (list_file and labels_file)
   raise "Files to be processed expected as arguments."
 end
 
-psalms = []
-
 pageref_optimizer = PageRefOptimal.new
 collect_labels(File.open(labels_file), pageref_optimizer)
 
@@ -225,8 +211,7 @@ input.each_pair do |s_title, section_content|
 
     occasion_content.each_pair do |h_title, h_content|
       hour_title h_title
-
-      psalms += hour_content h_content, pageref_optimizer
+      hour_content h_content, pageref_optimizer
     end
   end
 end
