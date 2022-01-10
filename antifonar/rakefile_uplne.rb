5906bb9dd1c5c5aad48072f49c4991c3458daf6c
Jakub Pavlik <severus@post.cz>
2022-01-10 21:47:26 +0100

    Compline booklet: build using typographus
    the only remaining issue is that preparatory syllables should not be
    highlighted, as the psalms are being sung to two different psalm tones
    and the preparatory syllables differ

--- antifonar/rakefile_uplne.rb
+++ antifonar/rakefile_uplne.rb
@@ -16,63 +16,9 @@ task :uvod => [ uvod_lytex ]
 ##############
 # kompletar
 
-adresar_kompletar = 'generovane/kompletar/'
-
-kompletar_options = $commonoptions
-
-kompletar_splitscores_command = $splitscores_command + " --output-dir #{adresar_kompletar} "
-
-file "vystup/antifonar_kompletar.pdf" => ["vystup/antifonar_kompletar.tex", "spolecne.tex"] do
-  chdir "vystup"
-  2.times { sh "pdflatex -shell-escape antifonar_kompletar" }
-  chdir ".."
-end
-
-cislazalmu_kompletar = [4, 134, 91, 86, 143, 31, 130, 16, 88]
-zalmy_kompletar = []
-cislazalmu_kompletar.each do |i|
-  zalmy_kompletar << genzalm("zalm#{i}.zalm", kompletar_options+" --accents 1:1", adresar_kompletar)
-end
-
-zalmy_kompletar << genzalm(
-  'zalm4.zalm',
-  $commonoptions_withoutdoxology +
-  $o_doxology_full +
-  ' --accents 1:1' +
-  ' --output-append "\vspace{1mm}\rubrikaPo{Toto zakončení Sláva Otci se připojuje na konci všech žalmů a kantik.}"',
-  adresar_kompletar
-)
-zalmy_kompletar << genzalm(
-  'kantikum_nuncdimittis.zalm',
-  $commonoptions_withoutdoxology +
-  $o_doxology_full +
-  ' --accents 2:1',
-  adresar_kompletar
-)
-
-multitask :zalmy_kompletar_multitask => zalmy_kompletar
-
-# noty:
-noty_kompletar = []
-
-[['../kompletar.ly', 'po'],
- ['../zakladni_napevy.ly', 'zacatek-bezny'],
- ['../pust_triduum.ly', 'ct-resp'],
- ['../velikonoce_velikonocnioktav.ly', 'resp'],
- ['../marianske_antifony.ly', 'salve']].each do |n|
-  nn = adresar_kompletar+'/'+File.basename(n[0]).gsub(/\.ly$/, '_'+n[1]+'.ly')
-  noty_kompletar << nn
-  file nn => [n[0]] do |t|
-    sh kompletar_splitscores_command + t.prerequisites.first
-  end
-end
-
-file "vystup/antifonar_kompletar.tex" => (["antifonar_kompletar.lytex", 'spolecne_antifonar.ly', :zalmy_kompletar_multitask]+noty_kompletar) do
-  sh "lilypond-book --output=vystup --pdf antifonar_kompletar.lytex"
-end
-
+kompletar_lytex = typographus('antifonar_kompletar.tytex')
 desc "Antiphonal for the Compline"
-task :kompletar => ["vystup/antifonar_kompletar.pdf"]
+task :kompletar => [kompletar_lytex]
 
 file 'vystup/antifonar_kompletar-broz.pdf' => ["vystup/antifonar_kompletar.pdf"] do |t|
   nws = t.prerequisites.first.gsub(/\..+$/, '') # input file without suffix
