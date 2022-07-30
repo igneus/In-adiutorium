28728c00b61d129f8c2a877792b693d08402257a
Jakub Pavlik <severus@post.cz>
2022-07-30 23:34:07 +0200

    typographus: for each score generate the png only once
    (when including scores as images rather than using lilypond-book)

--- nastroje/lib/typographus/score_strategy/graphicx.rb
+++ nastroje/lib/typographus/score_strategy/graphicx.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module Typographus
   module ScoreStrategy
     # Score strategy inserting in the document scores
@@ -6,6 +8,10 @@ module Typographus
     #   or converted to some other format using pandoc
     # Serious disadvantage: no support for page breaks in larger scores
     class Graphicx
+      def initialize
+        @png_generated = Set.new
+      end
+
       def output_filename(input_filename)
         input_filename.sub(/\.tytex$/, '.tex')
       end
@@ -14,10 +20,16 @@ module Typographus
         png_path = score_path.sub /\.ly$/, '.png'
         no_ext_path = score_path.sub /\.ly$/, ''
 
-        system("lilypond --png --output=#{no_ext_path} -dresolution=300 #{score_path}") ||
-          raise("failed to process #{score_path}")
-        system("mogrify -trim #{png_path}") ||
-          raise("failed to crop #{png_path}")
+        # Generating the images is time-consuming.
+        # When including the same score several times, generate it just once.
+        unless @png_generated.include? png_path
+          system("lilypond --png --output=#{no_ext_path} -dresolution=300 #{score_path}") ||
+            raise("failed to process #{score_path}")
+          system("mogrify -trim #{png_path}") ||
+            raise("failed to crop #{png_path}")
+
+          @png_generated << png_path
+        end
 
         "\\includegraphics{#{png_path}}"
       end
