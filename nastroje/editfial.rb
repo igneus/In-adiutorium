7f17f4313dc834ad98e988b73fbac39d46ae7bae
Jakub Pavlik <severus@post.cz>
2022-09-10 16:16:30 +0200

    editfial.rb: option to open development version of a score
    supports material of a single production file split into multiple
    development files

--- nastroje/editfial.rb
+++ nastroje/editfial.rb
@@ -1,21 +1,41 @@
 # editfial.rb <FIAL>
 # Opens the score specified by its FIAL in Frescobaldi.
 
+require 'optparse'
 require_relative 'fial'
 
 def grep_line_number(grep_output_line)
   grep_output_line.match(/\A(\d+)/) {|m| m[1].to_i }
 end
 
-def edit_fial(fial_str)
+def score_id_line_number(file_path, score_id)
+  grep_line_number `grep --line-number --max-count=1 'id = "#{score_id}"' #{file_path}`
+end
+
+def edit_fial(fial_str, variationes: false)
   line = 0
   if fial_str.include? ':'
     fial_str, line = fial_str.split(':', 2)
   end
 
   fial = FIAL.parse(fial_str)
-  id_line =
-    grep_line_number `grep --line-number --max-count=1 'id = "#{fial.id}"' #{fial.path}`
+  id_line = score_id_line_number fial.path, fial.id
+
+  (file, id_line) =
+    if variationes
+      files =
+        ['variationes/' + fial.path] +
+        Dir['variationes/' + fial.path.sub(/\.ly$/, '_*.ly')]
+
+      files
+        .collect {|f| [f, score_id_line_number(f, fial.id)] }
+        .find {|a,b| !b.nil? }
+    else
+      [
+        fial.path,
+        grep_line_number(`grep --line-number --max-count=1 'id = "#{fial.id}"' #{fial.path}`)
+      ]
+    end
 
   unless id_line
     STDERR.puts "could not find #{fial_str}"
@@ -23,16 +43,23 @@ def edit_fial(fial_str)
   end
 
   score_line =
-    grep_line_number(`head --lines #{id_line} #{fial.path} | grep --line-number '\\\\score' | tail --lines 1`) +
+    grep_line_number(`head --lines #{id_line} #{file} | grep --line-number '\\\\score' | tail --lines 1`) +
     line.to_i
 
-  `frescobaldi --line #{score_line} #{fial.path}`
+  `frescobaldi --line #{score_line} #{file}`
 end
 
 
 
+variationes = false
+OptionParser.new do |opts|
+  opts.on '-V', '--variationes', 'open development version of the score' do
+    variationes = true
+  end
+end.parse!
+
 STDERR.puts 'please specify FIAL' if ARGV.empty?
 
 ARGV.each do |fial|
-  edit_fial fial
+  edit_fial fial, variationes: variationes
 end
