e0c0e5a0df3c125759afea883a4a5a1a170a5ee5
Jakub Pavlik <severus@post.cz>
2022-10-15 12:46:48 +0200

    delete page breaks and some header fields

--- nastroje/empty_copy.rb
+++ nastroje/empty_copy.rb
@@ -7,9 +7,11 @@ puts \
   .read
   .gsub(/(?<=\\choralniRezim\n).+?(?=^\s*\})/m, '')
   .gsub(/(?<=\\addlyrics \{\n).+?(?=^\s*\})/m, '')
+  .gsub(/^\s*\\pageBreak.*?$/, '')
+  .gsub(/^\s*(placet|fial|fons|notitia|textus_approbatus) = ".*?".*?$/m, '')
   .gsub(/(?<key>\w+) = ".+?"/) {|str|
   m = Regexp.last_match
-  if %w(quid id).include? m[:key]
+  if %w(composer quid id).include? m[:key]
     str
   else
     "#{m[:key]} = \"\""
