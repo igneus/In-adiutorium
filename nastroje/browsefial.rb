6468f33dbf461264c468cdac6e331bd354a6f1b2
Jakub Pavlik <severus@post.cz>
2023-03-29 19:20:38 +0200

    browsefial.rb: option to open a listing
    instead of opening detail view of each specified score

--- nastroje/browsefial.rb
+++ nastroje/browsefial.rb
@@ -1,17 +1,42 @@
 # browsefial.rb <FIAL>
 # Opens the score specified by its FIAL in Adiutor (https://github.com/igneus/adiutor)
 
+require 'net/http'
+require 'optparse'
 require 'uri'
 
-ADIUTOR_URI = 'http://localhost:3000/chants/fial/'
+ADIUTOR_URI = 'http://localhost:3000/chants/fial'
 
 def browse_fial(fial)
-  uri = ADIUTOR_URI + URI.encode_www_form_component(fial)
+  uri = ADIUTOR_URI + '/' + URI.encode_www_form_component(fial)
   `xdg-open #{uri}`
 end
 
+def browse_fial_list(fials)
+  uri = URI(ADIUTOR_URI)
+  response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    req = Net::HTTP::Post.new ADIUTOR_URI
+    req.body = fials.join("\n")
+    http.request req
+  end
+  raise "unexpected response #{response}" if response.code != '302'
+  list_uri = response['Location']
+
+  `xdg-open #{list_uri}`
+end
+
+
+options = {}
+OptionParser.new do |opts|
+  opts.on '-l', '--list', 'Open scores in a listing'
+end.parse!(into: options)
 
 STDERR.puts 'please specify FIAL' if ARGV.empty?
+
+if options[:list]
+  browse_fial_list ARGV
+  exit
+end
 
 ARGV.each do |fial|
   browse_fial fial
