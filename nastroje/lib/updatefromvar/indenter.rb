64c4afc86e3f3c2a1736d3b749145654cee2a0b5
Jakub Pavlik <severus@post.cz>
2024-01-02 23:27:14 +0100

    simplify

--- nastroje/lib/updatefromvar/indenter.rb
+++ nastroje/lib/updatefromvar/indenter.rb
@@ -7,34 +7,26 @@ module Indenter
 
   # ensures that the lilypond score has the given indentation level
   def indent(lily_src, level)
-    if indentation_level(lily_src) != level
-      indent_change = (level - indentation_level(lily_src))
-      lily_src = lily_src.lines
-                 .collect {|l| indent_by(indent_change, l) }
-                 .join ''
-    end
+    indent_change = level - indentation_level(lily_src)
 
-    return lily_src
+    lily_src
+      .lines
+      .collect {|l| indent_by(indent_change, l) }
+      .join ''
   end
 
   # how many spaces is the lilypond score indented?
-  # computed as 'indentation of line 2' - 2 spaces
   def indentation_level(lily_src)
-    level = lily_src.lines[-1].index(/[^\s]/)
-    level = 0 if level < 0
-    return level
+    lily_src.lines[-1].index(/[^\s]/)
   end
 
   def indent_by(num, line)
     if num > 0 then
-      return (' ' * num) + line
-    elsif num < 0
-      abs_num = num.abs
-      if line[0..(abs_num-1)] =~ /^\s*$/
-        return line[abs_num..-1]
-      end
+      (' ' * num) + line
+    elsif num < 0 && line[0..(num.abs-1)] =~ /^\s*$/
+      line[num.abs..-1]
+    else
+      line
     end
-
-    return line
   end
 end
