igneus/In-adiutorium

commit e1b7f16db49e73be9dea3bba3c71e8f3b02c73cc

Commons: standardize titles, add section order information
to be used when generating the mobile app and the book

commune/commune_apostol.ly
@@ -1,8 +1,9 @@
 \version "2.14.2"
 
 \header {
-  title = "Společné texty o apoštolech"
+  title = \markup\titleCommune "O apoštolech"
   composer = "Jakub Pavlík"
+  book_order = 30
 }
 
 \include "../spolecne.ly"

commune/commune_evangelista.ly
@@ -1,8 +1,9 @@
 \version "2.15.37"
 
 \header {
-  title = "Společné texty o evangelistech"
+  title = \markup\titleCommune "O evangelistech"
   composer = "Jakub Pavlík"
+  book_order = 31
 }
 
 \include "../spolecne.ly"

commune/commune_jedenmucednik.ly
@@ -6,6 +6,7 @@
 \header {
   title = \markup\titleCommune "O jednom mučedníkovi nebo mučednici"
   composer = "Jakub Pavlík"
+  book_order = 50
 }
 
 \markup {\nadpisHodinka {"1. nešpory"}}

commune/commune_kostel.ly
@@ -6,6 +6,7 @@
 \header {
   title = \markup\titleCommune "O posvěcení kostela"
   composer = "Jakub Pavlík"
+  book_order = 10
 }
 
 \markup {\nadpisHodinka {"1. nešpory"}}

commune/commune_maria.ly
@@ -6,6 +6,7 @@
 \header {
   title = \markup\titleCommune{ \center-column { "O Panně Marii" "a sobotní památka Panny Marie" } }
   composer = "Jakub Pavlík"
+  book_order = 20
 }
 
 \markup {\nadpisHodinka {"1. nešpory"}}

commune/commune_panna.ly
@@ -6,6 +6,7 @@
 \header {
   title = \markup\titleCommune "O pannách"
   composer = "Jakub Pavlík"
+  book_order = 80
 }
 
 \markup {\nadpisHodinka {"1. nešpory"}}

commune/commune_pastyr.ly
@@ -6,6 +6,7 @@
 \header {
   title = \markup\titleCommune "O duchovních pastýřích"
   composer = "Jakub Pavlík"
+  book_order = 60
 }
 
 \markup {\nadpisHodinka {"1. nešpory"}}

commune/commune_reholnikatd.ly
@@ -6,6 +6,7 @@
 \header {
   % subtitle = "(Svatí muži a ženy - podskupiny)"
   composer = "Jakub Pavlík"
+  book_order = 110
 }
 
 \bookpart {

commit 9b7e7fdede9d106b979cd5e739952f66cb2a0d94

fix documents broken by e1b7f16d

commune/commune_apostol.ly
@@ -1,13 +1,13 @@
 \version "2.14.2"
 
+\include "../spolecne.ly"
+\include "../dilyresponsorii.ly"
+
 \header {
   title = \markup\titleCommune "O apoštolech"
   composer = "Jakub Pavlík"
   book_order = 30
 }
-
-\include "../spolecne.ly"
-\include "../dilyresponsorii.ly"
 
 \markup {\nadpisHodinka {"1. nešpory"}}
 

commune/commune_evangelista.ly
@@ -1,13 +1,13 @@
 \version "2.15.37"
 
+\include "../spolecne.ly"
+\include "../dilyresponsorii.ly"
+
 \header {
   title = \markup\titleCommune "O evangelistech"
   composer = "Jakub Pavlík"
   book_order = 31
 }
-
-\include "../spolecne.ly"
-\include "../dilyresponsorii.ly"
 
 \markup {\nadpisHodinka {"ranní chvály"}}
 