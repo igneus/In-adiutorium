ffc5644d6dee4878e34470d01df4a46c7625d0ec
Jakub Pavlik <severus@post.cz>
2026-05-24 18:03:43 +0200

    pray.rb: deal with the whole liturgical year

--- nastroje/pray.rb
+++ nastroje/pray.rb
@@ -24,13 +24,15 @@ end
 
 parser = OptionParser.new do |opts|
   opts.on '-d', '--dry-run', 'print command instead of executing it'
+  opts.on '--debug'
   opts.on '-y', '--yesterday'
   opts.on '-t', '--tomorrow'
+  opts.on '-I', '--[no-]interaction'
 end
 options = {}
-parser.parse ARGV, into: options
+args = parser.parse ARGV, into: options
 date =
-  ARGV[0]&.yield_self {|x| Date.parse x } ||
+  args[0]&.yield_self {|x| Date.parse x } ||
   if options[:yesterday]
     Date.today - 1
   elsif options[:tomorrow]
@@ -41,11 +43,14 @@ date =
 
 
 
-calendar = CR::PerpetualCalendar.new sanctorale: CR::Data['czech-praha-cs'].load_with_parents
+calendar = CR::PerpetualCalendar.new sanctorale: CR::Data['czech-praha-cs'].load_with_parents, temporale_options: {extensions: [CR::Temporale::Extensions::ChristEternalPriest]}
+ycalendar = calendar.calendar_for(date)
 day = calendar[date]
 celebration = day.celebrations.yield_self do |cs|
   if cs.size == 1
     cs.first
+  elsif false == options[:interaction]
+    cs[1] # take first which is not a ferial
   else
     HighLine.new.choose do |c|
       c.prompt = 'Please choose a celebration:'
@@ -75,6 +80,21 @@ COMMUNIA = [
   keys.collect {|k| [k, paths] }
 end.to_h
 
+DAY_PROPERS = {
+  ascension: 'velikonoce_nanebevstoupeni.pdf',
+  pentecost: 'velikonoce_seslaniduchasv.pdf',
+  mother_of_church: 'sanktoral/0598pmmatkycirkve.pdf',
+  christ_eternal_priest: 'mezidobi_kristakneze.pdf',
+  holy_trinity: 'mezidobi_trojice.pdf',
+  corpus_christi: 'mezidobi_telaakrvepane.pdf',
+  sacred_heart: 'mezidobi_nejsvsrdce.pdf',
+  immaculate_heart: 'sanktoral/0599neposkvrnenehosrdcepm.pdf',
+  all_souls: 'commune/zazemrele.pdf',
+  christ_king: 'mezidobi_kristakrale.pdf',
+
+  saturday_memorial_bvm: ['antifony.pdf', 'commune/commune_maria.pdf'],
+}
+
 # returns Array of all commune documents referenced by the specified
 # sanctorale document
 def extract_commons(sanctorale_ly)
@@ -90,7 +110,7 @@ end
 # accepts an AppropriatedAntiphons::Celebration instance,
 # returns an Array which may or may not contain a path
 # to an ad hoc generated document with appropriated antiphons for the occasion
-def appropriated_antiphons(data)
+def appropriated_antiphons(data, dry_run: false)
   aa = data.antiphons
   return [] if aa.empty?
 
@@ -102,7 +122,7 @@ def appropriated_antiphons(data)
     subtitle = \"(ad hoc)\"
   }
   LY
-  spawn "{ echo '#{header}'; ruby nastroje/getfial.rb #{aa.join(' ')}; } | lilypond --output=#{path.split('.')[0]} -"
+  spawn "{ echo '#{header}'; ruby nastroje/getfial.rb #{aa.join(' ')}; } | lilypond --output=#{path.split('.')[0]} -" unless dry_run
 
   [path]
 end
@@ -110,35 +130,46 @@ end
 c = celebration
 skip_psalter_on_sunday = lambda {|d| c.sunday? ? d[1..-1] : d }
 docs =
-  if c.cycle == :temporale
+  if DAY_PROPERS[c.symbol]
+    [DAY_PROPERS[c.symbol]].flatten
+  elsif c.cycle == :temporale
     if c.symbol == :easter_sunday ||
        (day.season == CR::Seasons::EASTER && (day.season_week == 1 || (day.season_week == 2 && date.sunday?)))
       ['velikonoce_velikonocnioktav.pdf']
     elsif day.season == CR::Seasons::TRIDUUM
       ['pust_triduum.pdf']
-    elsif day.season == CR::Seasons::LENT && day.season_week == 6
-      ['pust_svatytyden.pdf']
-    elsif c.sunday? || c.ferial?
-      case day.season
-      when CR::Seasons::ADVENT
-        %w(antifony.pdf advent_responsoria.pdf advent_antifony.pdf)
-          .yield_self do |d|
-          if date.day >= 17
-            d[1..-1]
-          else
-            skip_psalter_on_sunday.(d)
-          end
-        end
-      when CR::Seasons::LENT
+    elsif day.season == CR::Seasons::EASTER
+      %w(velikonoce_zaltar.pdf velikonoce_responsoria.pdf velikonoce_antifony.pdf)
+        .yield_self(&skip_psalter_on_sunday)
+    elsif day.season == CR::Seasons::LENT
+      if day.season_week == 6
+        ['pust_svatytyden.pdf']
+      else
         %w(antifony.pdf pust_responsoria.pdf pust_antifony.pdf)
           .yield_self(&skip_psalter_on_sunday)
-      when CR::Seasons::EASTER
-        %w(velikonoce_zaltar.pdf velikonoce_responsoria.pdf velikonoce_antifony.pdf)
-          .yield_self(&skip_psalter_on_sunday)
-      when CR::Seasons::ORDINARY
-        %w(antifony.pdf responsoria.pdf).yield_self do |d|
-          c.sunday? ? (d + ['mezidobi_nedele.pdf']) : d
+      end
+    elsif day.season == CR::Seasons::CHRISTMAS
+      if date <= Date.new(ycalendar.year + 1, 1, 1)
+        ['vanoce_narozenipane.pdf']
+      elsif date >= Date.new(ycalendar.year + 1, 1, 6)
+        ['vanoce_zjevenipane.pdf']
+      elsif date.sunday?
+        ['vanoce_druhanedele.pdf']
+      else
+        ['antifony.pdf', 'vanoce_narozenipane.pdf', 'vanoce_ferie.pdf']
+      end
+    elsif day.season == CR::Seasons::ADVENT
+      %w(antifony.pdf advent_responsoria.pdf advent_antifony.pdf)
+        .yield_self do |d|
+        if date.day >= 17
+          d[1..-1]
+        else
+          skip_psalter_on_sunday.(d)
         end
+      end
+    elsif day.season == CR::Seasons::ORDINARY
+      %w(antifony.pdf responsoria.pdf).yield_self do |d|
+        c.sunday? ? (d + ['mezidobi_nedele.pdf']) : d
       end
     end
   else
@@ -163,17 +194,21 @@ docs =
           .each.find {|i| i.date =~ date }
 
       if entry
-        entry.communia.flat_map do |kw|
+        (entry.communia&.flat_map do |kw|
           COMMUNIA[kw.to_sym] || [commune_path(kw)]
-        end + appropriated_antiphons(entry)
+        end || []) + appropriated_antiphons(entry, dry_run: options[:'dry-run'])
       end
     end
   end
 
 
 
+if options[:debug]
+  STDERR.puts "#{date.to_s} #{celebration.symbol}"
+end
 if docs.nil?
   STDERR.puts "Cannot determine sheet music for '#{c}' yet."
+  exit 1
 else
   cmd = ['bash', 'nastroje/open.sh', *docs]
   if options[:'dry-run']
